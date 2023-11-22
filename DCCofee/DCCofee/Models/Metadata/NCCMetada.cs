a77e580ce369cf2bcde2e78f5d01641ad8ca0fb7
nguyenkhaccuong07072003 <124782853+nguyenkhaccuong07072003@users.noreply.github.com>
2023-11-22 10:59:04 +0700

    init

---
+++ DCCofee/DCCofee/Models/Metadata/NCCMetada.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DCCofee.Models.Metadata
+{
+    [MetadataTypeAttribute(typeof(NCCMetadata))]
+    public partial class NCCMetada
+    {
+        internal class NCCMetadata
+        {
+            public int Id { get; set; }
+            [DisplayName("Tên nhà cung cấp")]
+            [Required(ErrorMessage = "{0} Hãy nhập tên nhà cung cấp")]
+            public string TenNCC { get; set; }
+
+            public string DiaChi { get; set; }
+            public string Sdt { get; set; }
+            public string Email { get; set; }
+
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+            public virtual ICollection<PHIEUNHAP> PHIEUNHAP { get; set; }
+        }
+    }
+
+
+}
