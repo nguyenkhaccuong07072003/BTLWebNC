adf63557b676669b137fc0dc68344e82f95abd2a
Junnngggg <luongthuydung1803@gmail.com>
2023-11-24 23:03:38 +0700

    init

---
+++ DCCofee/DCCofee/Controllers/DanhMucController.cs
@@ -0,0 +1,20 @@
+﻿using DCCofee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DCCofee.Controllers
+{
+    public class DanhMucController : Controller
+    {
+        // GET: DanhMuc
+        QLQCFEntities db = new QLQCFEntities();
+        public ActionResult Index()
+        {
+            List<SanPham> sp = db.SanPham.ToList();
+            return View(sp);
+        }
+    }
+}
